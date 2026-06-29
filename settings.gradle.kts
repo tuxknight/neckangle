6d3a2cc4094c33f6db94a43afb75c4f761989df2
awx-fuyuanchu <fuyuanchu@gmail.com>
2026-06-29 09:47:59 +0800

    fix: dependencyResolutionManagement API

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
